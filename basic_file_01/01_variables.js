e3cce3a51601ef6a196064cd7312506edb83c151
tanvirmandal <tanvirmasudmandal@gmail.com>
2026-07-11 15:56:01 +0530

    datatypes

--- basic_file_01/01_variables.js
+++ basic_file_01/01_variables.js
@@ -1,7 +1,14 @@
-const accountId="12345"
-let email="rah@gmail.com"
-var accountPassword ="93#lko"
-accountCity="jaipur"
+// const accountId="12345"
+// let email="rah@gmail.com"
+// var accountPassword ="93#lko"
+// accountCity="jaipur"
+// accountState;
 
-accountId=2;
-console.log(accountId);
+//accountId=2;
+// console.log(accountId);
+let email="abc@gmail.com"
+var accountPassword = "OOOOOOO"
+accountCity = "Masagram"
+let accountState;
+
+console.table([email,accountPassword,accountCity,accountState])
